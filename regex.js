550b25db9a6c86fa1f3b1630d852ec993c705edc
Nick Osman <Nick.Osman@softwire.com>
2022-08-10 16:16:09 +0100

    creates dictionary of domains

--- regex.js
+++ regex.js
@@ -5,9 +5,17 @@ const data = fs.readFileSync('C:/Users/nicosm/work/regex/test.txt', 'utf8');
 
 function findEmail(){
 
-    var numberOfDomains = data.match(/@[A-Za-z0-9-]*(\.[A-Za-z0-9-]*)+/g).length
-    return numberOfDomains
+    domains = data.match(/@[A-Za-z0-9-]*(\.[A-Za-z0-9-]*)+/g)
+    uniqueDomains = [... new Set(domains)]
+    domainDictionary = {}
 
+    for (let i = 0; i < uniqueDomains.length; i++){
+        var currentDomain = uniqueDomains[i]
+        var numberOfOccurances = domains.filter(x => x == currentDomain).length
+        domainDictionary[currentDomain] = numberOfOccurances
+    }
+
+    return domainDictionary
 }
 
 console.log(findEmail())
