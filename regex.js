7d9a350bd54355f6cec4f5465d34329fbd768603
Nick Osman <Nick.Osman@softwire.com>
2022-08-10 17:12:40 +0100

    prints highest frequency domains

--- regex.js
+++ regex.js
@@ -1,9 +1,20 @@
 const fs = require('fs');
-
+const readline = require('readline-sync');
 const data = fs.readFileSync('C:/Users/nicosm/work/regex/test.txt', 'utf8');
 
+function getIntWithPrompt(prompt){
 
-function findEmail(){
+    console.log(prompt)
+    answer = parseInt(readline.prompt())
+    while (!(Number.isInteger(answer))){
+        console.log("That was not an integer, please try again")
+        answer = Number(readline.prompt())
+    }
+    return answer
+}
+
+
+function createDomainDictionary(){
 
     domains = data.match(/@[A-Za-z0-9-]*(\.[A-Za-z0-9-]*)+/g)
     uniqueDomains = [... new Set(domains)]
@@ -18,4 +29,26 @@ function findEmail(){
     return domainDictionary
 }
 
-console.log(findEmail())
+
+function sortDomainsByFrequency(dictionary){
+
+    var domainArray = Object.keys(dictionary).map(key => {return [key, dictionary[key]]})
+    var domainArray = domainArray.sort((a, b) => b[1] - a[1])
+    return domainArray
+}
+
+
+function getDomains(){
+    
+    var uniqueDomains = createDomainDictionary();
+    var sortedDomains = sortDomainsByFrequency(uniqueDomains);
+    console.log("The 10 most common domains are:")
+    console.log(sortedDomains.slice(0, 10))
+
+    var minFrequency = getIntWithPrompt("Enter a minimum frequency: ");
+    domainsAboveMinFrequency = sortedDomains.filter(x => x[1] > minFrequency)
+    console.log(domainsAboveMinFrequency)
+}
+
+
+getDomains()
