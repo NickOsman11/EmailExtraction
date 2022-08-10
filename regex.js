48678281e5160d387535ff998b86502a3e1d1ef8
Nick Osman <Nick.Osman@softwire.com>
2022-08-10 15:30:19 +0100

    changed to regex

--- regex.js
+++ regex.js
@@ -2,10 +2,12 @@ const fs = require('fs');
 
 const data = fs.readFileSync('C:/Users/nicosm/work/regex/test.txt', 'utf8');
 
-var count = 0
-for (let i = 0; i<data.length - 13; i++){
-    if (data.slice(i, i + 13) == "@softwire.com"){
-        count += 1
-    }
+
+function findEmail(){
+
+    var numberOfEmails = data.match(/@softwire.com/g).length
+    return numberOfEmails
+
 }
-console.log(count)
+
+console.log(findEmail())
